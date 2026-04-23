5574d878487f3ff351d572b7bfbbd4c3e13f644f
Brianna-Graham12 <149191474+Brianna-Graham12@users.noreply.github.com>
2026-04-23 17:27:37 -0400

    Implement MazeNode class for maze representation
    Added MazeNode class to represent a point in the maze with a label and life points.

--- MazeNode.java
+++ MazeNode.java
@@ -1,1 +1,60 @@
 # Project5
+package Project5; 
+
+/** 
+Represenst a single pointin the maze
+
+each node must have a label used for BST ordering 
+life points that hero can collect 
+
+*/
+
+public class MazeNode implements Comparable<MazeNode> {
+
+  private String label;  
+
+  // unique identifier 
+
+  private int lifePoints; 
+
+  // points gained at node 
+
+  // Constructor with label and points 
+
+  public MazeNode(String label, int lifePoints) {
+    this.label = label; 
+    this.lifePoints = lifePoints; 
+    
+  }
+  
+  // Returns label of node
+
+  public String getLabel(){
+
+    return label;
+  }
+
+// returns lifePoints 
+
+  public int getLifePoints(){
+
+    return lifePoints;
+  }
+
+  // Compare nnodes based on label alphabet ordering 
+
+  @Override 
+  public int compareTo(MazeNode other) {
+    return this.label.compareTo(other.label);
+  }
+
+  // Returns String
+
+  @Override
+  public String toString() {
+
+    return label + " " + lifePoints;
+  }
+
+  
+}
